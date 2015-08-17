2813b3b3f5837f83ecf6a3fa6b52a5cb54ce86a4
Edgar Li <liedgar7@gmail.com>
2015-08-17 18:07:06 -0400

    Added host pictures

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -8,49 +8,49 @@ $(function() {
       adaptiveHeight: true
     });
   // FadeIn Slogan and Download Button
-  $('h3.banner-message').hide()
-  $('h3.banner-message').fadeIn(3000, function() {
+  $('h3.banner-message').hide();
+  $('h3.banner-message').fadeIn(1500, function() {
       $(".beta-access-button").animate({
         opacity: 1
       }, 1000);
   });
-  // Scrolling Magic
-  var controller = new ScrollMagic.Controller();
-
-  var fadeInFirstEvent = new TweenMax.to('#first-event-box', 1, {
-    opacity: 1
-  });
-
-  var fadeInSecondEvent = new TweenMax.to('#second-event-box', 1, {
-    opacity: 1
-  });
-
-  var fadeInThirdEvent = new TweenMax.to('#third-event-box', 1, {
-    opacity: 1
-  });
-
-  // Scene Handler
-  var fade_first_event_scene = new ScrollMagic.Scene({
-      offset: 50,
-      reverse: false // allows the effect to trigger when scrolled in the reverse direction
-    })
-    .setTween(fadeInFirstEvent)
-    // .addIndicators()
-    .addTo(controller);
-
-  var fade_second_event_scene = new ScrollMagic.Scene({
-      offset: 200,
-      reverse: false // allows the effect to trigger when scrolled in the reverse direction
-    })
-    .setTween(fadeInThirdEvent)
-    // .addIndicators()
-    .addTo(controller);
-
-  var fade_third_event_scene = new ScrollMagic.Scene({
-      offset: 400,
-      reverse: false // allows the effect to trigger when scrolled in the reverse direction
-    })
-    .setTween(fadeInSecondEvent)
-    // .addIndicators()
-    .addTo(controller);
+  // // Scrolling Magic
+  // var controller = new ScrollMagic.Controller();
+  //
+  // var fadeInFirstEvent = new TweenMax.to('#first-event-box', 1, {
+  //   opacity: 1
+  // });
+  //
+  // var fadeInSecondEvent = new TweenMax.to('#second-event-box', 1, {
+  //   opacity: 1
+  // });
+  //
+  // var fadeInThirdEvent = new TweenMax.to('#third-event-box', 1, {
+  //   opacity: 1
+  // });
+  //
+  // // Scene Handler
+  // var fade_first_event_scene = new ScrollMagic.Scene({
+  //     offset: 50,
+  //     reverse: false // allows the effect to trigger when scrolled in the reverse direction
+  //   })
+  //   .setTween(fadeInFirstEvent)
+  //   // .addIndicators()
+  //   .addTo(controller);
+  //
+  // var fade_second_event_scene = new ScrollMagic.Scene({
+  //     offset: 200,
+  //     reverse: false // allows the effect to trigger when scrolled in the reverse direction
+  //   })
+  //   .setTween(fadeInThirdEvent)
+  //   // .addIndicators()
+  //   .addTo(controller);
+  //
+  // var fade_third_event_scene = new ScrollMagic.Scene({
+  //     offset: 400,
+  //     reverse: false // allows the effect to trigger when scrolled in the reverse direction
+  //   })
+  //   .setTween(fadeInSecondEvent)
+  //   // .addIndicators()
+  //   .addTo(controller);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hypelist' });
+  res.render('index', {
+    title: 'Hypelist'
+  });
 });
 
 /* GET beta signup page. */
 router.get('/betarequest', function(req, res, next) {
-  res.render('betarequest', { title: 'Hypelist - Beta Access' });
+  res.render('betarequest', {
+    title: 'Hypelist - Beta Access'
+  });
 });
 
 /* GET terms & conditions page. */
 router.get('/terms', function(req, res, next) {
-  res.render('terms', { title: 'Terms & Conditions' });
+  res.render('terms', {
+    title: 'Terms & Conditions'
+  });
 });
 
 /* GET privacy page. */
 router.get('/privacy', function(req, res, next) {
-  res.render('privacy', { title: 'Privacy' });
+  res.render('privacy', {
+    title: 'Privacy'
+  });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Us' });
+  res.render('contact', {
+    title: 'Contact Us'
+  });
+});
+
+/* POST contact page. */
+router.post('/submitcontact', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'edgar@thehypelist.co',
+      pass: '1ampvCapital'
+    }
+  });
+
+  // Mail Options
+  var mailOptions = {
+    from: req.body.firstname + '&lt;' + req.body.email + '&gt;', //grab from data from the request body object
+    to: 'edgar@thehypelist.co',
+    subject: 'Contact Hypelist',
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    // Email not sent
+    if (error) {
+      return console.log(error);
+      res.render('contact', {
+        title: 'Contact Us',
+        msg: 'Error occured, message not sent.',
+        err: true,
+        page: 'contact'
+      })
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.render('contact', {
+        title: 'Contact Us',
+        msg: 'Thanks for emailing us!',
+        err: false,
+        page: 'contact'
+      })
+    }
+  })
 });
 
 module.exports = router;
