c7c61967e796e3a72ff94424f6727a19c87b43b6
Edgar Li <liedgar7@gmail.com>
2016-06-16 14:45:43 -0400

    facebook login works

--- public/javascripts/login-service.js
+++ public/javascripts/login-service.js
@@ -25,12 +25,30 @@ $(function() {
     }(document, 'script', 'facebook-jssdk'));
 
   $("#facebook-button").click(function() {
-    Parse.FacebookUtils.logIn(null, {
+    Parse.FacebookUtils.logIn("public_profile, user_photos, email,user_friends", {
       success: function(user) {
         if (!user.existed()) {
           alert("User signed up and logged in through Facebook!");
         } else {
-          alert("User logged in through Facebook!");
+          FB.api('/me',{fields:'id,first_name,last_name,name,gender,verified,email,picture.type(large)'}, function(response) {
+                if (!response.error) {
+                  // We save the data on the Parse user
+                  user.set("fbId", response.id);
+                  user.set("firstName", response.first_name);
+                  user.set("lastName", response.last_name);
+                  user.set("gender", response.gender);
+                  user.set("fbEmail", response.email);
+                  user.set("image", response.picture.type(large));
+                  user.save(null, {
+                    success: function(user) {
+                      
+                    },
+                    error: function(user, error) {
+                      console.log("Oops, something went wrong saving your account.");
+                    }
+                  });
+                }
+              });
         }
       },
       error: function(user, error) {
