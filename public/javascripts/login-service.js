975ccf8bea8e061e8cd4b8ef80c60813a93fc40f
Edgar Li <liedgar7@gmail.com>
2016-06-17 11:36:27 -0400

    Clearing up stuff

--- public/javascripts/login-service.js
+++ public/javascripts/login-service.js
@@ -8,30 +8,30 @@ $(function() {
 
     $("#registration-step-2").show();
 
-    // Parse.initialize("5t3F1S3wKnVGIKHob1Qj0Je3sygnFiwqAu6PP400", "NyZCP6peg3Si9VwUYLZdCRMAj62xoNBxOMOgv76M");
-    //
-    // window.fbAsyncInit = function() {
-    //     Parse.FacebookUtils.init({ // this line replaces FB.init({
-    //         appId: '678431492288292', // Facebook App ID
-    //         status: true, // check Facebook Login status
-    //         cookie: true, // enable cookies to allow Parse to access the session
-    //         xfbml: true, // initialize Facebook social plugins on the page
-    //         version: 'v2.6' // point to the latest Facebook Graph API version
-    //     });
-    //
-    //     // Run code after the Facebook SDK is loaded.
-    // };
-    //
-    // (function(d, s, id) {
-    //     var js, fjs = d.getElementsByTagName(s)[0];
-    //     if (d.getElementById(id)) {
-    //         return;
-    //     }
-    //     js = d.createElement(s);
-    //     js.id = id;
-    //     js.src = "//connect.facebook.net/en_US/sdk.js";
-    //     fjs.parentNode.insertBefore(js, fjs);
-    // }(document, 'script', 'facebook-jssdk'));
+    Parse.initialize("5t3F1S3wKnVGIKHob1Qj0Je3sygnFiwqAu6PP400", "NyZCP6peg3Si9VwUYLZdCRMAj62xoNBxOMOgv76M");
+
+    window.fbAsyncInit = function() {
+        Parse.FacebookUtils.init({ // this line replaces FB.init({
+            appId: '678431492288292', // Facebook App ID
+            status: true, // check Facebook Login status
+            cookie: true, // enable cookies to allow Parse to access the session
+            xfbml: true, // initialize Facebook social plugins on the page
+            version: 'v2.6' // point to the latest Facebook Graph API version
+        });
+
+        // Run code after the Facebook SDK is loaded.
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
     $("#facebook-button").click(function() {
         Parse.FacebookUtils.logIn("public_profile, user_photos, email,user_friends", {
@@ -68,25 +68,25 @@ $(function() {
             }
         });
     });
-
-    $('#submit').on('click', function() {
-        var currentUser = Parse.User.current();
-        var email = $('#email-input').val();
-        var phoneNumber = $('#phoneNumber-input').val();
-
-        if (email !== "" && phoneNumber !== "") {
-            currentUser.set("email", email);
-            currentUser.set("phoneNumber", phoneNumber);
-            currentUser.save(null, {
-                success: function(user) {
-                    alert("Saved user phone number.");
-                },
-                error: function(user, error) {
-                    console.log("Oops, something went wrong saving your account.");
-                }
-            });
-        }
-    });
+    //
+    // $('#submit').on('click', function() {
+    //     var currentUser = Parse.User.current();
+    //     var email = $('#email-input').val();
+    //     var phoneNumber = $('#phoneNumber-input').val();
+    //
+    //     if (email !== "" && phoneNumber !== "") {
+    //         currentUser.set("email", email);
+    //         currentUser.set("phoneNumber", phoneNumber);
+    //         currentUser.save(null, {
+    //             success: function(user) {
+    //                 alert("Saved user phone number.");
+    //             },
+    //             error: function(user, error) {
+    //                 console.log("Oops, something went wrong saving your account.");
+    //             }
+    //         });
+    //     }
+    // });
 
 
 });
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var Parse = require('parse/node');
+var Event = Parse.Object.extend("Event");
+var AdmissionOption = Parse.Object.extend("AdmissionOption");
+var Location = Parse.Object.extend("Location");
 
 Parse.initialize("5t3F1S3wKnVGIKHob1Qj0Je3sygnFiwqAu6PP400",
-"NyZCP6peg3Si9VwUYLZdCRMAj62xoNBxOMOgv76M,NUwTuaL9aqcGkgFc0MUrng4SdQz9RPDcEudMvUGZ");
+    "NyZCP6peg3Si9VwUYLZdCRMAj62xoNBxOMOgv76M", "NUwTuaL9aqcGkgFc0MUrng4SdQz9RPDcEudMvUGZ");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,8 +25,93 @@ router.get('/signup', function(req, res, next) {
 });
 
 /* GET beta access page. */
-router.post('/login', function(req, res, next) {
+router.post('/save-user', function(req, res, next) {
+    console.log(req.body);
+    var currentUser = Parse.User.current();
+    var email = req.body.email;
+    // var phoneNumber = req.body.phoneNumber;
 
+    if (email !== "") {
+        return res.redirect('/');
+        //
+        // currentUser.set("email", email);
+        // // currentUser.set("phoneNumber", phoneNumber);
+        // currentUser.save(null, {
+        //     success: function(user) {
+        //       return res.redirect('/');
+        //     },
+        //     error: function(user, error) {
+        //         console.log("Oops, something went wrong saving your account.");
+        //     }
+        // });
+    }
+});
+
+router.get('/events', function(req, res, next) {
+    var Event = Parse.Object.extend("Event");
+    var query = new Parse.Query(Event);
+    query.limit(10);
+    query.include("location");
+    query.addDescending("date");
+    return query.find({
+        success: function(results) {
+            return res.render('events', {
+                events: results,
+                user: req.user
+            });
+        },
+        error: function(error) {
+            return next(error);
+        }
+    });
+});
+
+router.get('/event/:id', function(req, res, next) {
+    var event, admissionOptions;
+
+    var eventID = req.params.id;
+    var query = new Parse.Query(Event);
+    query.include("location");
+    query.get(eventID).then(function(fetchedEvent) {
+        event = fetchedEvent;
+
+        var admissionQuery = new Parse.Query(AdmissionOption);
+        var location = new Location();
+        location.id = event.get('location').id;
+        admissionQuery.equalTo("location", location);
+        return admissionQuery.find().then(null, function(error) {
+            return next(err);
+        });
+    }).then(function(admissionQuery) {
+        admissionOptions = admissionQuery;
+        return res.render('event', {
+            event: event,
+            admissionOptions: admissionOptions,
+            user: req.user
+        });
+    }).done();
+});
+
+router.get('/event/:eventID/:admissionOptionID/checkout', function(req, res, next) {
+    var admissionOptionID = req.params.admissionOptionID;
+    var eventID = req.params.eventID;
+    var query = new Parse.Query(Event);
+    query.include("location");
+    query.get(eventID).then(function(fetchedEvent) {
+        console.log('fetched event is ' + JSON.stringify(fetchedEvent));
+        event = fetchedEvent;
+
+        var query = new Parse.Query(AdmissionOption);
+        query.include("location");
+        query.get(admissionOptionID).then(function(admissionOption) {
+            console.log('fetched admissionOption is ' + JSON.stringify(admissionOption));
+            return res.render('checkout', {
+                event: event,
+                admissionOption: admissionOption,
+                user: req.user
+            });
+        }).done();
+    });
 });
 
 /* GET beta access page. */
