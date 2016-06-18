1a454ffcea62e823ea8e8af66bf54139d09f7739
Edgar Li <liedgar7@gmail.com>
2016-06-17 20:54:34 -0400

    felony

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Parse = require('parse/node');
 var Event = Parse.Object.extend("Event");
 var AdmissionOption = Parse.Object.extend("AdmissionOption");
 var Location = Parse.Object.extend("Location");
-var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+var stripe = require('stripe')('sk_live_brHKisyZ82Yyizrzba5pw78X');
 var PNF = require('google-libphonenumber').PhoneNumberFormat;
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
