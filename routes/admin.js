584a53d39f3ca0195e73d5335ef9349fc71e3cbe
Edgar Li <liedgar7@gmail.com>
2016-07-25 17:19:07 -0400

    limit guy tickets to 62

--- routes/admin.js
+++ routes/admin.js
@@ -17,23 +17,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/event/:id', function(req, res, next) {
-    var event, completedTransactions;
-
+    var event, completedTransactions, maleAdmissions, femaleAdmissions;
     var eventID = req.params.id;
     var query = new Parse.Query(Event);
     query.include("location");
     query.get(eventID).then(function(fetchedEvent) {
         event = fetchedEvent;
-        var completedTransactionQuery = new Parse.Query(CompletedTransaction);
-        completedTransactionQuery.equalTo("event", event);
-        return completedTransactionQuery.limit(1000).find().then(null, function(error) {
+        var maleAdmissionQuery = new Parse.Query(CompletedTransaction);
+        maleAdmissionQuery.equalTo("event", event);
+        maleAdmissionQuery.equalTo("description", "Male Admission Ticket ");
+        return maleAdmissionQuery.limit(62).find().then(null, function(error) {
             return next(err);
         });
-    }).then(function(completedTransactionQuery) {
-        completedTransactions = completedTransactionQuery;
+    }).then(function(maleAdmissionQuery) {
+        maleAdmissions = maleAdmissionQuery;
+        var femaleAdmissionQuery = new Parse.Query(CompletedTransaction);
+        femaleAdmissionQuery.equalTo("event", event);
+        femaleAdmissionQuery.equalTo("description", "Female Admission Ticket ");
+        return femaleAdmissionQuery.limit(1000).find().then(null, function(error) {
+            return next(err);
+        });
+    }).then(function(femaleAdmissionQuery) {
+        femaleAdmissions = femaleAdmissionQuery;
+        completedTransactions = femaleAdmissions.concat(maleAdmissions);
+        console.log(completedTransactions);
+
         return res.render('admin/event', {
             event: event,
             completedTransactions: completedTransactions,
+            maleAdmissions: maleAdmissions,
+            femaleAdmissions: femaleAdmissions,
             user: req.user
         });
     }).done();
