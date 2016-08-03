419dced9266d74b955749e770c5a6ed4d0788702
Edgar Li <liedgar7@gmail.com>
2016-08-03 19:48:08 -0400

    Changed to moment timezone

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.locals.moment = require('moment');
+app.locals.moment = require('moment-timezone');
 app.set('port', process.env.PORT || 3000);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
